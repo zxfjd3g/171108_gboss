e190759556ce7857639dc0f81a7a3f81c0d92c7c
zxfjd3g <258147149@qq.com>
2018-03-17 11:03:13 +0800

    底部导航

---
+++ src/components/nav-footer/nav-footer.jsx
@@ -0,0 +1,43 @@
+/*
+底部导航的UI组件
+ */
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import {TabBar} from 'antd-mobile'
+import {withRouter} from 'react-router-dom'
+
+const Item = TabBar.Item
+
+class NavFooter extends React.Component {
+
+  static propTypes = {
+    navList: PropTypes.array.isRequired
+  }
+
+  render() {
+    // nav.hide = true/false hide代表当前项应该被隐藏
+    const navList = this.props.navList.filter(nav => !nav.hide) // 回调函数返回值为true, 当前元素就会留下, 否则不留
+    // 当前请求的路径
+    const {pathname} = this.props.location
+    return (
+      <TabBar>
+        {
+          navList.map((nav, index) => (
+            <Item key={nav.path}
+                  title={nav.text}
+                  icon={{uri: require(`./imgs/${nav.icon}.png`)}}
+                  selectedIcon={{uri: require(`./imgs/${nav.icon}-active.png`)}}
+                  selected={pathname===nav.path}
+                  onPress={() => {
+                    this.props.history.replace(nav.path)
+                  }}
+            />
+          ))
+        }
+      </TabBar>
+    )
+  }
+}
+
+export default withRouter(NavFooter) // 让非路由组件可以访问到路由组件的API
--- src/containers/dashboard/dashboard.jsx
+++ src/containers/dashboard/dashboard.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 import {Route, Switch, Redirect} from 'react-router-dom'
 import cookies from 'browser-cookies' // get(), set()
 import {connect} from 'react-redux'
+import {NavBar} from 'antd-mobile'
 
 import BossInfo from '../boss-info/boss-info'
 import GeniusInfo from '../genius-info/genius-info'
@@ -12,13 +13,45 @@ import Genius from '../genius/genius'
 import Boss from '../boss/boss'
 import Msg from '../msg/msg'
 import User from '../user/user'
-import NotFound from '../not-found/not-found'
+import NotFound from '../../components/not-found/not-found'
+import NavFooter from '../../components/nav-footer/nav-footer'
 
 import {getUser} from '../../redux/actions'
 import {getRedirectPath} from '../../utils'
 
 class Dashboard extends React.Component {
-
+// 组件类和组件对象
+  // 给组件对象添加属性
+  navList = [
+    {
+      path: '/boss', // 路由路径
+      component: Boss,
+      title: '牛人列表',
+      icon: 'boss',
+      text: '牛人',
+    },
+    {
+      path: '/genius', // 路由路径
+      component: Genius,
+      title: 'BOSS列表',
+      icon: 'job',
+      text: 'BOSS',
+    },
+    {
+      path: '/msg', // 路由路径
+      component: Msg,
+      title: '消息列表',
+      icon: 'msg',
+      text: '消息',
+    },
+    {
+      path: '/user', // 路由路径
+      component: User,
+      title: '个人中心',
+      icon: 'user',
+      text: '我',
+    }
+  ]
 
   componentDidMount () {
     // cookie中有userid
@@ -32,6 +65,8 @@ class Dashboard extends React.Component {
 
 
   render () {
+    // 得到当前请求的path
+    const pathname = this.props.location.pathname
     // 判断用户是否已登陆(过)(cookie中userid是否有值)
     const userid = cookies.get('userid')
     if(!userid) { // 如果没值, 自动跳转到登陆界面
@@ -41,28 +76,40 @@ class Dashboard extends React.Component {
     // redux中的user是否有数据
     const {user} = this.props
     if(!user._id) {
-      return null
+      return null  // 不做任何显示
     } else {
       // 请求根路径时, 自动 跳转到对应的用户主界面
-      const pathname = this.props.location.pathname
       if(pathname==='/') {
         const path = getRedirectPath(user.type, user.avatar)
         return <Redirect to={path}/>
       }
+
+      // 指定哪个nav应该被隐藏
+      if(user.type==='boss') {
+        this.navList[1].hide = true
+      } else {
+        this.navList[0].hide = true
+      }
     }
+
+    // 得到当前的nav
+    const currentNav = this.navList.find(nav => nav.path===pathname)
 
     return (
       <div>
-        <p>{this.props.user.type}</p>
+        {currentNav? <NavBar>{currentNav.title}</NavBar> : null}
         <Switch>
           <Route path='/bossinfo' component={BossInfo}></Route>
           <Route path='/geniusinfo' component={GeniusInfo}></Route>
+
           <Route path='/genius' component={Genius}></Route>
           <Route path='/boss' component={Boss}></Route>
           <Route path='/msg' component={Msg}></Route>
           <Route path='/user' component={User}></Route>
           <Route component={NotFound}></Route>
         </Switch>
+
+        {currentNav?  <NavFooter navList={this.navList}/>: null}
       </div>
     )
   }
